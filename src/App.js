bbb19cf0ae61895ffc8e79ac4c94a9ffd88a0864
Michelle <116389047+dataMichelle@users.noreply.github.com>
2024-08-07 12:51:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import TodoList from "./component/TodoList";
+import TodoInput from "./component/TodoInput";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    setTodos(newTodoList);
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
+  }
+
+  return (
+    <>
+      <TodoInput
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        handleAddTodos={handleAddTodos}
+      />
+      <TodoList
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+        todos={todos}
+      />
+    </>
+  );
+}
+
+export default App;
